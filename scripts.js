855906afbd7362025c27158e837321670443154b
Rohit Negi <rohitnegicap@gmail.com>
2019-05-18 13:47:03 +0530

    1st commit

---
+++ scripts.js
@@ -0,0 +1,70 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo()
+{
+    navigator.mediaDevices.getUserMedia({video:true, audio: false}).then(localMediaStream => {
+        console.log(localMediaStream);
+        video.srcObject = localMediaStream;
+        video.play();
+        paintToCanvas();
+    })
+}
+function paintToCanvas()
+{
+    const width = video.videoWidth;
+    const Height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = Height;
+    
+    return setInterval(()=> {
+        ctx.drawImage(video,0,0,width,Height);
+        let pixels = ctx.getImageData(0,0,width,Height);
+   // pixels = rgbSplit(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.putImageData(pixels,0,0);
+    },16);
+    
+    
+}
+function takePhoto()
+{
+    snap.currentTime =0;
+    snap.play();
+    
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man"/>`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels)
+{
+    for(let i = 0; i < pixels.data.length; i+=4)
+        {
+            pixels.data[i+0]=pixels.data[i+0]+200;
+            pixels.data[i+1]=pixels.data[i+1]-50;
+            pixels.data[i+2]=pixels.data[i+2]*0.5;
+            
+        }
+    return pixels;
+}
+function rgbSplit(pixels)
+{
+    for(let i = 0; i < pixels.data.length; i+=4)
+        {
+            pixels.data[i-150]=pixels.data[i+0];
+            pixels.data[i+100]=pixels.data[i+1];
+            pixels.data[i-150]=pixels.data[i+0];
+            
+        }
+    return pixels;
+}
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas)
